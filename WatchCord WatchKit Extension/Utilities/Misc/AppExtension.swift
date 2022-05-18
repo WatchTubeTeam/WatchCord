de21ebdfea48a86c4602138fbe49dfa4f6107dc8
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-05-18 18:33:07 +0100

    remove for now

--- WatchCord WatchKit Extension/Utilities/Misc/AppExtension.swift
+++ WatchCord WatchKit Extension/Utilities/Misc/AppExtension.swift
@@ -12,25 +12,25 @@ extension WatchCordApp {
     #warning("This needs rewriting")
     static func error(_ error: Error? = nil, text: String? = nil, additionalDescription: String? = nil, reconnectOption _: Bool = true) {
         DispatchQueue.main.async {
-            let alert = NSAlert()
-            alert.messageText = error?.localizedDescription ?? text ?? "Unknown error occured"
-            if let additionalDescription = additionalDescription {
-                alert.informativeText = additionalDescription
-            }
-            alert.addButton(withTitle: "OK")
-            if wss != nil {
-                alert.addButton(withTitle: "Reconnect")
-                alert.addButton(withTitle: "Force reconnect")
-            }
-            alert.alertStyle = .warning
-            guard let window = NSApplication.shared.mainWindow else { return }
-            alert.beginSheetModal(for: window) { res in
-                if res == .alertSecondButtonReturn {
+//            let alert = NSAlert()
+//            alert.messageText = error?.localizedDescription ?? text ?? "Unknown error occured"
+//            if let additionalDescription = additionalDescription {
+//                alert.informativeText = additionalDescription
+//            }
+//            alert.addButton(withTitle: "OK")
+//            if wss != nil {
+//                alert.addButton(withTitle: "Reconnect")
+//                alert.addButton(withTitle: "Force reconnect")
+//            }
+//            alert.alertStyle = .warning
+            
+//            alert.beginSheetModal(for: window) { res in
+//                if res == .alertSecondButtonReturn {
                     wss?.reset()
-                } else if res == .alertThirdButtonReturn {
-                    wss?.hardReset()
-                }
-            }
+//                } else if res == .alertThirdButtonReturn {
+//                    wss?.hardReset()
+//                }
+//            }
         }
     }
 }
